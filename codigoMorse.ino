a147d36846d066fea41c681b1d9f63471220181b
Merazord <jose.angel.meraz@gmail.com>
2019-10-04 15:03:29 -0500

    se agrego funcion de escribir en la consola

--- codigoMorse.ino
+++ codigoMorse.ino
@@ -1,291 +1,306 @@
 int pin = 2;
+int pin2 = 3;
 int corto = 200;
 int largo = 600;
 
 void setup()
 {
-    pinMode(pin, OUTPUT);
+  Serial.begin(9600);
+  pinMode(pin, OUTPUT);
 }
 
 void pulsoLargo()
 {
-    digitalWrite(pin, HIGH);
-    delay(largo);
-    digitalWrite(pin, LOW);
-    delay(corto);
+  digitalWrite(pin, HIGH);
+  delay(largo);
+  digitalWrite(pin, LOW);
+  delay(corto);
 }
 
 void pulsoCorto()
 {
-    digitalWrite(pin, HIGH);
-    delay(corto);
-    digitalWrite(pin, LOW);
-    delay(corto);
+  digitalWrite(pin, HIGH);
+  delay(corto);
+  digitalWrite(pin, LOW);
+  delay(corto);
+}
+
+void pulsoInicial()
+{
+  digitalWrite(pin2, HIGH);
+  delay(3000);
+  digitalWrite(pin2, LOW);
+  delay(3000);
 }
 
 void pulsoFinal()
 {
-    digitalWrite(pin, LOW);
-    delay(corto * 2);
+  digitalWrite(pin, LOW);
+  delay(corto * 2);
 }
 
 void pulsoEspacio()
 {
-    digitalWrite(pin, LOW);
-    delay(largo * 3);
+  digitalWrite(pin, LOW);
+  delay(largo * 3);
 }
 
 void morse(char letra)
 {
-    switch (letra)
-    {
-    case 'a':
-        pulsoCorto();
-        pulsoLargo();
-        break;
+  switch (letra)
+  {
+  case 'a':
+    pulsoCorto();
+    pulsoLargo();
+    break;
 
-    case 'b':
-        pulsoLargo();
-        pulsoCorto();
-        pulsoCorto();
-        pulsoCorto();
-        break;
+  case 'b':
+    pulsoLargo();
+    pulsoCorto();
+    pulsoCorto();
+    pulsoCorto();
+    break;
 
-    case 'c':
-        pulsoLargo();
-        pulsoCorto();
-        pulsoLargo();
-        pulsoCorto();
-        break;
+  case 'c':
+    pulsoLargo();
+    pulsoCorto();
+    pulsoLargo();
+    pulsoCorto();
+    break;
 
-    case 'd':
-        pulsoLargo();
-        pulsoCorto();
-        pulsoCorto();
-        break;
+  case 'd':
+    pulsoLargo();
+    pulsoCorto();
+    pulsoCorto();
+    break;
 
-    case 'e':
-        pulsoCorto();
-        break;
+  case 'e':
+    pulsoCorto();
+    break;
 
-    case 'f':
-        pulsoCorto();
-        pulsoCorto();
-        pulsoLargo();
-        pulsoCorto();
-        break;
+  case 'f':
+    pulsoCorto();
+    pulsoCorto();
+    pulsoLargo();
+    pulsoCorto();
+    break;
 
-    case 'g':
-        pulsoLargo();
-        pulsoLargo();
-        pulsoCorto();
-        break;
+  case 'g':
+    pulsoLargo();
+    pulsoLargo();
+    pulsoCorto();
+    break;
 
-    case 'h':
-        pulsoCorto();
-        pulsoCorto();
-        pulsoCorto();
-        pulsoCorto();
-        break;
+  case 'h':
+    pulsoCorto();
+    pulsoCorto();
+    pulsoCorto();
+    pulsoCorto();
+    break;
 
-    case 'i':
-        pulsoCorto();
-        pulsoCorto();
-        break;
+  case 'i':
+    pulsoCorto();
+    pulsoCorto();
+    break;
 
-    case 'j':
-        pulsoCorto();
-        pulsoLargo();
-        pulsoLargo();
-        pulsoLargo();
-        break;
+  case 'j':
+    pulsoCorto();
+    pulsoLargo();
+    pulsoLargo();
+    pulsoLargo();
+    break;
 
-    case 'k':
-        pulsoLargo();
-        pulsoCorto();
-        pulsoLargo();
-        break;
+  case 'k':
+    pulsoLargo();
+    pulsoCorto();
+    pulsoLargo();
+    break;
 
-    case 'l':
-        pulsoCorto();
-        pulsoLargo();
-        pulsoCorto();
-        pulsoCorto();
-        break;
+  case 'l':
+    pulsoCorto();
+    pulsoLargo();
+    pulsoCorto();
+    pulsoCorto();
+    break;
 
-    case 'm':
-        pulsoLargo();
-        pulsoLargo();
-        break;
+  case 'm':
+    pulsoLargo();
+    pulsoLargo();
+    break;
 
-    case 'n':
-        pulsoLargo();
-        pulsoCorto();
-        break;
+  case 'n':
+    pulsoLargo();
+    pulsoCorto();
+    break;
 
-    case 'o':
-        pulsoLargo();
-        pulsoLargo();
-        pulsoLargo();
-        break;
+  case 'o':
+    pulsoLargo();
+    pulsoLargo();
+    pulsoLargo();
+    break;
 
-    case 'p':
-        pulsoCorto();
-        pulsoLargo();
-        pulsoLargo();
-        pulsoCorto();
-        break;
+  case 'p':
+    pulsoCorto();
+    pulsoLargo();
+    pulsoLargo();
+    pulsoCorto();
+    break;
 
-    case 'q':
-        pulsoLargo();
-        pulsoLargo();
-        pulsoCorto();
-        pulsoLargo();
-        break;
+  case 'q':
+    pulsoLargo();
+    pulsoLargo();
+    pulsoCorto();
+    pulsoLargo();
+    break;
 
-    case 'r':
-        pulsoCorto();
-        pulsoLargo();
-        pulsoCorto();
-        break;
+  case 'r':
+    pulsoCorto();
+    pulsoLargo();
+    pulsoCorto();
+    break;
 
-    case 's':
-        pulsoCorto();
-        pulsoCorto();
-        pulsoCorto();
-        break;
+  case 's':
+    pulsoCorto();
+    pulsoCorto();
+    pulsoCorto();
+    break;
 
-    case 't':
-        pulsoLargo();
-        break;
+  case 't':
+    pulsoLargo();
+    break;
 
-    case 'u':
-        pulsoCorto();
-        pulsoCorto();
-        pulsoLargo();
-        break;
+  case 'u':
+    pulsoCorto();
+    pulsoCorto();
+    pulsoLargo();
+    break;
 
-    case 'v':
-        pulsoCorto();
-        pulsoCorto();
-        pulsoCorto();
-        pulsoLargo();
-        break;
+  case 'v':
+    pulsoCorto();
+    pulsoCorto();
+    pulsoCorto();
+    pulsoLargo();
+    break;
 
-    case 'w':
-        pulsoCorto();
-        pulsoLargo();
-        pulsoLargo();
-        break;
+  case 'w':
+    pulsoCorto();
+    pulsoLargo();
+    pulsoLargo();
+    break;
 
-    case 'x':
-        pulsoLargo();
-        pulsoCorto();
-        pulsoCorto();
-        pulsoLargo();
-        break;
+  case 'x':
+    pulsoLargo();
+    pulsoCorto();
+    pulsoCorto();
+    pulsoLargo();
+    break;
 
-    case 'y':
-        pulsoLargo();
-        pulsoCorto();
-        pulsoLargo();
-        pulsoLargo();
-        break;
+  case 'y':
+    pulsoLargo();
+    pulsoCorto();
+    pulsoLargo();
+    pulsoLargo();
+    break;
 
-    case 'z':
-        pulsoLargo();
-        pulsoLargo();
-        pulsoCorto();
-        pulsoCorto();
-        break;
+  case 'z':
+    pulsoLargo();
+    pulsoLargo();
+    pulsoCorto();
+    pulsoCorto();
+    break;
 
-    case ' ':
-        pulsoEspacio();
-        break;
+  case ' ':
+    pulsoEspacio();
+    break;
 
-    case '0':
-        pulsoLargo();
-        pulsoLargo();
-        pulsoLargo();
-        pulsoLargo();
-        pulsoLargo();
+  case '0':
+    pulsoLargo();
+    pulsoLargo();
+    pulsoLargo();
+    pulsoLargo();
+    pulsoLargo();
 
-    case '1':
-        pulsoCorto();
-        pulsoLargo();
-        pulsoLargo();
-        pulsoLargo();
-        pulsoLargo();
+  case '1':
+    pulsoCorto();
+    pulsoLargo();
+    pulsoLargo();
+    pulsoLargo();
+    pulsoLargo();
 
-    case '2':
-        pulsoCorto();
-        pulsoCorto();
-        pulsoLargo();
-        pulsoLargo();
-        pulsoLargo();
+  case '2':
+    pulsoCorto();
+    pulsoCorto();
+    pulsoLargo();
+    pulsoLargo();
+    pulsoLargo();
 
-    case '3':
-        pulsoCorto();
-        pulsoCorto();
-        pulsoCorto();
-        pulsoLargo();
-        pulsoLargo();
+  case '3':
+    pulsoCorto();
+    pulsoCorto();
+    pulsoCorto();
+    pulsoLargo();
+    pulsoLargo();
 
-    case '4':
-        pulsoCorto();
-        pulsoCorto();
-        pulsoCorto();
-        pulsoCorto();
-        pulsoLargo();
+  case '4':
+    pulsoCorto();
+    pulsoCorto();
+    pulsoCorto();
+    pulsoCorto();
+    pulsoLargo();
 
-    case '5':
-        pulsoCorto();
-        pulsoCorto();
-        pulsoCorto();
-        pulsoCorto();
-        pulsoCorto();
+  case '5':
+    pulsoCorto();
+    pulsoCorto();
+    pulsoCorto();
+    pulsoCorto();
+    pulsoCorto();
 
-    case '6':
-        pulsoLargo();
-        pulsoCorto();
-        pulsoCorto();
-        pulsoCorto();
-        pulsoCorto();
+  case '6':
+    pulsoLargo();
+    pulsoCorto();
+    pulsoCorto();
+    pulsoCorto();
+    pulsoCorto();
 
-    case '7':
-        pulsoLargo();
-        pulsoLargo();
-        pulsoCorto();
-        pulsoCorto();
-        pulsoCorto();
+  case '7':
+    pulsoLargo();
+    pulsoLargo();
+    pulsoCorto();
+    pulsoCorto();
+    pulsoCorto();
 
-    case '8':
-        pulsoLargo();
-        pulsoLargo();
-        pulsoLargo();
-        pulsoCorto();
-        pulsoCorto();
+  case '8':
+    pulsoLargo();
+    pulsoLargo();
+    pulsoLargo();
+    pulsoCorto();
+    pulsoCorto();
 
-    case '9':
-        pulsoLargo();
-        pulsoLargo();
-        pulsoLargo();
-        pulsoLargo();
-        pulsoCorto();
-    }
+  case '9':
+    pulsoLargo();
+    pulsoLargo();
+    pulsoLargo();
+    pulsoLargo();
+    pulsoCorto();
+  }
 }
 
 void loop()
 {
-    char palabra[] = "hola mundo";
-    int contador = strlen(palabra);
+  if (Serial.available() > 0)
+  {
+    String palabra = Serial.readString();
+    Serial.print("Palabra: ");
+    Serial.println(palabra);
+    pulsoInicial();
     char letra;
-    for (int i = 0; i <= contador; i++)
+    for (int i = 0; i <= palabra.length(); i++)
     {
-        letra = palabra[i];
-        morse(letra);
-        if (i != contador)
-            pulsoFinal();
+      letra = palabra[i];
+      Serial.println(letra);
+      morse(letra);
+      if (i != palabra.length())
+        pulsoFinal();
     }
-    pulsoEspacio();
+  }
 }
